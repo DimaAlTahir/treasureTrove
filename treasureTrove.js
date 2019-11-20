ba7e12d57f19fc49eeed41dd7c3b72d89c9d9bcf
dnitsios <dnitsios@ulcan.ac.uk>
2019-11-20 17:01:26 +0200

    create the js of the quiz

---
+++ treasureTrove.js
@@ -0,0 +1,22 @@
+let challengesElement = document.getElementById("treasureHuntChallenges");
+
+
+fetch("https://codecyprus.org/th/api/list")
+    .then(response => response.json())
+    .then(jsonObject =>
+    {
+        let treasureHuntsArray = jsonObject.treasureHunts;
+
+        for (let i = 0; i < treasureHuntsArray.length; i++) {
+            let listChallenge = document.createElement("li");
+            listChallenge.innerHTML = "<a href='https://codecyprus.org/th/api/start?player=jimk&app=TreasureTroveApp&treasure-hunt-id=" + treasureHuntsArray[i].uuid + "'>" + treasureHuntsArray[i].name + "</a>";
+            challengesElement.appendChild(listChallenge);
+        }
+    });
+
+
+// for(let i = 0; i < json.treasureHunts.length; i++) {
+//     let name = json.treasureHunts[i].name;
+//     let uuid = json.treasureHunts[i].uuid;
+// t.innerHTML += "<li>" + name + " (<a href='javascript:start(\"" + uuid + "\")'>" + uuid + "</a>)</li>"
+// }
