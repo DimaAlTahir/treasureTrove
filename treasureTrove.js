9592c6c1bad5ac6e2c979486917bdedca3c847fb
DimaAlTahir <daaal-tahir@uclan.ac.uk>
2019-11-27 14:20:57 +0200

    updated html & css to reflect js elements

--- treasureTrove.js
+++ treasureTrove.js
@@ -1,3 +1,27 @@
+const question=document.getElementById('question');
+const answer=document.getElementById('answer');
+const startbtn=document.getElementById('start');
+const nextbtn=document.getElementById('next');
+const skipbtn=document.getElementById('skip');
+
+startbtn.addEventListener('click', startQuiz)
+nextbtn.addEventListener('click', ()=>{
+    current++;
+    callNextQuestion()
+})
+
+function startQuiz(){
+    startbtn.classList.add('hide')
+    fetch("https://codecyprus.org/th/api/question?session=ag9nfmNvZGVjeXBydXNvcmdyFAsSB1Nlc3Npb24YgICAoMa0gQoM")
+        .then(response => response.json())
+        .then(jsonObject =>{})
+    callNextQuestion()
+}
+
+function callNextQuestion(){
+
+}
+
 let challengesElement = document.getElementById("treasureHuntChallenges");
 
 
@@ -20,3 +44,4 @@ fetch("https://codecyprus.org/th/api/list")
 //     let uuid = json.treasureHunts[i].uuid;
 // t.innerHTML += "<li>" + name + " (<a href='javascript:start(\"" + uuid + "\")'>" + uuid + "</a>)</li>"
 // }
+
