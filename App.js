847e09af87ef630d50095be9e72cfd77465695e0
dnitsios <dnitsios@uclan.ac.uk>
2019-12-10 20:36:37 +0200

    Finished the Questions table and added a small description in the end discribing my goals of doing Unit Testing

--- App.js
+++ App.js
@@ -78,11 +78,3 @@ function goToIndex() {
 function restartGame() {
     setTimeout(function(){window.location.href="Signin.html?restart";},500)
 }
-// function urlify(text) {
-//     var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-//     //var urlRegex = /(https?:\/\/[^\s]+)/g;
-//     return text.replace(urlRegex, function(url,b,c) {
-//         var url2 = (c == 'www.') ?  'http://' +url : url;
-//         return '<a href="' +url2+ '" target="_blank">' + url + '</a>';
-//     })
-// }
